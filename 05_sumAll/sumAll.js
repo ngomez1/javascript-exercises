c8753ce37059007428981a157dd4516c5070c45d
ngomez1 <n.a.gomez.005@gmail.com>
2022-08-17 17:40:18 -0500

    Completed Javascript assignments 5-7 for The Odin Project

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+    if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR'
+    }
+    let sum = 0;
+    let x,y;
+    if (num1 > num2) {
+        x = num2;
+        y = num1;
+    }
+    else {
+        x = num1;
+        y = num2;
+    }
+    for (let i = x; i <= y; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
