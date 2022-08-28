1ad1cbce8ff8421e8032b192e3927c90399ab409
ngomez1 <n.a.gomez.005@gmail.com>
2022-08-28 18:58:31 -0500

    Completed assignment from The Odin Project

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const cleaned = a.replace(/[^a-z]/gi, "").toLowerCase();
+    let revArray = [];
+    for (const item of cleaned) {
+        revArray.unshift(item);
+    }
+    return revArray.join('') === cleaned;
 };
 
 // Do not edit below this line
