ba25cb1dea80512de057e9e753801e12764fdee1
ngomez1 <n.a.gomez.005@gmail.com>
2022-08-28 19:17:10 -0500

    Completed assignment from The Odin Project

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if(a<1){
+        return "OOPS"
+    }
 
+    let sequence = [1,1];
+    let count = a > 2 ? 2 : a;
+    while (count < a) {
+        sequence.push(sequence[count-1] + sequence[count-2]);
+        count++;
+    }
+    return sequence[a-1];
 };
 
 // Do not edit below this line
