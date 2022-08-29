a5b59a986895d2e953d06721d5dde73ba2797d8e
ngomez1 <n.a.gomez.005@gmail.com>
2022-08-28 19:43:54 -0500

    Completed an assignment from The Odin Project

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objs) {
+    objs.map(function(obj) {
+        if(!obj["yearOfDeath"]) {
+            obj["yearOfDeath"] = (new Date()).getFullYear();
+        }
+    });
+    return objs.sort((a,b) => a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1)[0];
 };
 
 // Do not edit below this line
